681358d3a65a137b68f69a621833dfce8ace5263
Italo Pedroza <74671728+italoknd@users.noreply.github.com>
2022-05-08 04:12:46 -0400

    na luta

--- semaforo.js
+++ semaforo.js
@@ -1,22 +1,24 @@
 let red = document.getElementById('red')
 let yellow = document.getElementById('yellow')
 let green = document.getElementById('green')
-let semaforo = document.getElementById("semaforo")
+let semaforo = document.getElementById('semaforo')
+let automatic = document.getElementById('automatic')
 
-red.addEventListener("click", vermelho)
-yellow.addEventListener("click", amarelo)
-green.addEventListener("click", verde)
+red.addEventListener('click', vermelho)
+yellow.addEventListener('click', amarelo)
+green.addEventListener('click', verde)
+automatic.addEventListener('click', automatico)
 
 function vermelho() {
-  semaforo.src = "./imgs/vermelho.png"
+  semaforo.src = './imgs/vermelho.png'
 }
 
 function amarelo() {
-  semaforo.src = "./imgs/amarelo.png"
+  semaforo.src = './imgs/amarelo.png'
 }
 
 function verde() {
-  semaforo.src = "./imgs/verde.png"
+  semaforo.src = './imgs/verde.png'
 }
 
 function automatico() {
